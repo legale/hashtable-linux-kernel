91a3d9e23bcc086815d1953a1471b6af8c4f2fa0
Isaev Ruslan <legale.legale@gmail.com>
2024-02-11 21:43:40 +0300

    more tests
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- test/test_create_hashtable.c
+++ test/test_create_hashtable.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <time.h>
 
 #include "hashtable.h"
 #include "unity.h"
@@ -124,42 +125,111 @@ void test_delete_string_from_hashtable(void) {
 }
 
 void test_add_and_delete_entries(void) {
-    hashtable_t *ht = create_hashtable(10, free_entry); 
-    TEST_ASSERT_NOT_NULL(ht);
+  hashtable_t *ht = create_hashtable(10, free_entry);
+  TEST_ASSERT_NOT_NULL(ht);
 
-    size_t entries_before, entries_after, entries_count;
-    const int num_entries = 1000;
-    char str_buffer[40];
+  size_t entries_before, entries_after, entries_count;
+  const int num_entries = 1000;
+  char str_buffer[40];
 
-    // add entries to ht
-    for (int i = 0; i < num_entries; i++) {
-        snprintf(str_buffer, sizeof(str_buffer), "string%d", i);
-        add_string_to_hashtable(ht, str_buffer);
-    }
+  // add entries to ht
+  for (int i = 0; i < num_entries; i++) {
+    snprintf(str_buffer, sizeof(str_buffer), "string%d", i);
+    add_string_to_hashtable(ht, str_buffer);
+  }
 
-    // count entries and check
-    COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_count);
-    TEST_ASSERT_EQUAL_UINT32(num_entries, entries_count);
+  // count entries and check
+  COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_count);
+  TEST_ASSERT_EQUAL_UINT32(num_entries, entries_count);
 
-    // delete entries and check
-    for (int i = 0; i < num_entries; i++) {
-        snprintf(str_buffer, sizeof(str_buffer), "string%d", i);
+  // delete entries and check
+  for (int i = 0; i < num_entries; i++) {
+    snprintf(str_buffer, sizeof(str_buffer), "string%d", i);
 
-        // count entries before deletion
-        COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_before);
+    // count entries before deletion
+    COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_before);
 
-        int delete_result = delete_string_from_hashtable(ht, str_buffer);
-        TEST_ASSERT_EQUAL_INT(0, delete_result); // ok
+    int delete_result = delete_string_from_hashtable(ht, str_buffer);
+    TEST_ASSERT_EQUAL_INT(0, delete_result); // ok
 
-        // count entries after deletion
-        COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_after);
+    // count entries after deletion
+    COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_after);
 
-        // check 
-        TEST_ASSERT_EQUAL_UINT32(entries_before - 1, entries_after);
-    }
+    // check
+    TEST_ASSERT_EQUAL_UINT32(entries_before - 1, entries_after);
+  }
 
-    // free ht entries and ht itself
-    FREE_HASHTABLE(ht, string_entry_t, node, free_entry);
+  // free ht entries and ht itself
+  FREE_HASHTABLE(ht, string_entry_t, node, free_entry);
+}
+
+void test_add_delete_performance(void) {
+	uint32_t ht_bits = 15;
+  hashtable_t *ht = create_hashtable(ht_bits, free_entry);
+  printf("ht size: %u\n", 1 << ht->bits);
+  TEST_ASSERT_NOT_NULL(ht);
+
+  // заполняем четверть макс. емкости, чтобы минимизировать коллизии ключей
+  size_t entries = (1 << ht->bits) / 4;
+  printf("entries to add: %zu\n", entries);
+
+  char test_str_prefix[] = "string";
+  struct timespec start, end;
+  long long time_used_for_one[500], time_used_for_all, average_time_for_all;
+  size_t i, j;
+
+  // Измеряем среднее время добавления 1 записи
+  for (j = 0; j < 500; ++j) {
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    char str[30];
+    snprintf(str, sizeof(str), "%s%zu", test_str_prefix, j);
+    add_string_to_hashtable(ht, str);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time_used_for_one[j] = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+  }
+	//очищаем таблицу
+	FREE_HASHTABLE(ht, string_entry_t, node, free_entry);
+	
+	//создаем заново
+	ht = create_hashtable(ht_bits, free_entry);
+
+  // Вычисляем среднее время для одной записи и сравниваем
+  long long total_time_for_one = 0;
+  for (i = 0; i < 500; ++i) {
+    // printf("%lld\n", time_used_for_one[i]);
+    total_time_for_one += time_used_for_one[i];
+  }
+  long long avg_time_for_one = total_time_for_one / 500;
+
+  // Наполняем таблицу и измеряем общее время добавления
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  for (i = 0; i < entries; ++i) {
+    char str[30];
+    snprintf(str, sizeof(str), "%s%zu", test_str_prefix, i);
+    add_string_to_hashtable(ht, str);
+  }
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  time_used_for_all = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+
+  average_time_for_all = time_used_for_all / entries;
+  printf("average_time_for_one: %lldns average_time_for_all: %lldns\n", avg_time_for_one, average_time_for_all);
+
+  if (average_time_for_all > avg_time_for_one * 1.5) {
+    TEST_FAIL_MESSAGE("Performance degradation: adding entries took longer than expected (> 50%).");
+  }
+
+  // Очищаем хэш-таблицу
+  for (i = 0; i < entries; ++i) {
+    char str[30];
+    snprintf(str, sizeof(str), "string%zu", i);
+    delete_string_from_hashtable(ht, str);
+  }
+
+  size_t entries_count = 0;
+  COUNT_ENTRIES_IN_HASHTABLE(ht, string_entry_t, node, entries_count);
+  TEST_ASSERT_EQUAL_UINT64(0, entries_count);
+
+  FREE_HASHTABLE(ht, string_entry_t, node, free_entry);
 }
 
 int main(void) {
@@ -167,6 +237,7 @@ int main(void) {
   RUN_TEST(test_create_hashtable);
   RUN_TEST(test_add_to_ht);
   RUN_TEST(test_delete_string_from_hashtable);
-	RUN_TEST(test_add_and_delete_entries);
+  RUN_TEST(test_add_and_delete_entries);
+  RUN_TEST(test_add_delete_performance);
   return UNITY_END();
 }
